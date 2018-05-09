01e2c97457d3f262c632bb5f1eaad104ef6f7477
Medhi REDJEM <external.z03mredj@decathlon.com>
2018-05-09 20:08:12 +0200

    :construction: Modify deploy process

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git config --global user.name "Travis CI"
 git config --global user.email "travis@travis-ci.org"
 
 git checkout -b gh-pages
-git add . dist/*.md
+git add . dist/*
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
 git remote add origin-pages https://${GITHUB_API_KEY}@github.com/mredjem/community-day.git > /dev/null 2>&1
