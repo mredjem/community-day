7e5d61733c985496bb0e2fbbdeb7e9feb759eb26
Medhi REDJEM <external.z03mredj@decathlon.com>
2018-05-11 18:40:32 +0200

    :green_heart: Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 git checkout master
-git subtree split --prefix dist -b gh-pages
+git subtree split --prefix src -b gh-pages
 git push -f "https://${GITHUB_API_KEY}@github.com/mredjem/community-day.git" gh-pages:gh-pages
 git branch -D gh-pages
