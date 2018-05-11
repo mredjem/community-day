d6d9ae9757844381fe45d623428b6f35d1c2c582
Medhi REDJEM <external.z03mredj@decathlon.com>
2018-05-11 18:27:23 +0200

    :green_heart: Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git config --global user.name "Travis CI"
-git config --global user.email "travis@travis-ci.org"
+git config --global user.email "$AUTHOR_EMAIL"
 
 git checkout -b gh-pages
 git rm -rf .
@@ -9,5 +9,5 @@ git push origin gh-pages
 
 git checkout master
 git subtree split --prefix dist -b gh-pages
-git push -f origin gh-pages:gh-pages
+git push -f "https://${GITHUB_API_KEY}@github.com/mredjem/community-day.git" gh-pages:gh-pages
 git branch -D gh-pages
